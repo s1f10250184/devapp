c8bbd00430331c87b2c4a0c6bf704669a9f96de3
s1f10250184 <s1f102501844@iniad.org>
2025-11-21 14:02:50 +0900

    Ubuntu入れるのもいいけどまずはこっちを完成させろ

--- moodmane/src/App.jsx
+++ moodmane/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Recode() {
+
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,16 +15,16 @@ function App() {
       <div>
         <h1>気分記録</h1>
           <div className="happy">
-            <button>😊</button>
+            <button type="button" id="1">😊</button>
           </div>
           <div className="sad">
-            <button>😭</button>
+            <button type="button" id="2">😭</button>
           </div>
           <div className="angry">
-            <button>😠</button>
+            <button type="button" id="3">😠</button>
           </div>
           <div className="fantastic">
-            <button>🤩</button>
+            <button type="button" id="4">🤩</button>
           </div>
       </div>
       <p className="read-the-docs">
