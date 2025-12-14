fda553e518306e20755574b9090ecc3702b6e9c8
s1f10250184 <s1f102501844@iniad.org>
2025-12-14 14:35:54 +0900

    backendいじったから次はreactというかfrontendについてやるぞーー、Zenn見ながらゆっくり作業し ようかな

--- backend/mood/models.py
+++ backend/mood/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Mood(models.Model):
+    MOOD_CHOICES = [
+        ("happy", "happy"),
+        ("sad", "sad"),
+        ("angry", "angry"),
+        ("fantastic", "fantastic"),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mood = models.CharField(max_length=10, choices=MOOD_CHOICES)
     created_at = models.DateTimeField(auto_now_add = True)
     note = models.TextField()
--- moodmane/src/App.jsx
+++ moodmane/src/App.jsx
@@ -15,16 +15,16 @@ function App() {
       <div>
         <h1>気分記録</h1>
           <div className="happy">
-            <button type="button" id="1">😊</button>
+            <button type="button" onClick={() => selectMood("happy")}>😊</button>
           </div>
           <div className="sad">
-            <button type="button" id="2">😭</button>
+            <button type="button" onClick={() => selectMood("sad")}>😭</button>
           </div>
           <div className="angry">
-            <button type="button" id="3">😠</button>
+            <button type="button" onClicl={() => selectMood("angry")}>😠</button>
           </div>
           <div className="fantastic">
-            <button type="button" id="4">🤩</button>
+            <button type="button" onClick={() => selectMood("fantastic")}>🤩</button>
           </div>
       </div>
       <p className="read-the-docs">
